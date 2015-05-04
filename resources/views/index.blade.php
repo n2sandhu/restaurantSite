8dc1199b455d51d787711b6e1cde4b7412199d64
NAVJEET <navi_sandhu2013@yahoo.com.au>
2015-05-04 10:57:03 +1000

    index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Thai Mook- Restauarant</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav style="margin-bottom:0px" class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Thai Mook Restaurant</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Booking</a></li>
+        <li><a href="#">Menu</a></li>
+      </ul>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search Website">
+        </div>
+        <button type="submit" class="btn btn-default">Go</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Login</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img style="width:100%; height:400px" src="images/slider2.jpeg" alt="...">
+      <div class="carousel-caption">
+        Experience the authentic Thai Flavour
+      </div>
+    </div>
+    <div class="item">
+      <img style="width:100%" src="images/slider3.jpg" alt="...">
+      <div class="carousel-caption">
+        Experience the authentic Thai Flavour
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row written">
+	<div class="col-lg-4 first">
+		<strong>Why Dine With Us?</strong>
+		<p>bla bla</p>
+	</div>
+	<div class=" col-lg-4 second">
+		<strong>Feel The Difference</strong>
+		<p>bla bla</p>
+	</div>
+	<div class=" col-lg-4 third">
+	<strong>Customer Reviews</strong>
+		<p>bla bla</p>
+	</div>
+</div>
+<div class="row bookingText">
+	<strong>GOT DINNER PLANS??? DINE WITH THE BEST</strong>
+</div>
+<div class="row booking">
+	<strong><a href="">BOOK NOW!!!</a> </strong>
+</div>
+</body>
+</html>
